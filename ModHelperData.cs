04971c5157efea4c742c734f7068acbdd6d9b130
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-09-19 13:30:30 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace LegoFactory;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "38.3";
+    public const string Version = "1.0.0";
+    public const string Name = "LegoFactory";
+
+    public const string Description = "Spike arent good lego are better";
+
+    public const string RepoOwner = "Mattcy1"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "LegoFactory"; // TODO add your repo name here, also in the download url in README.md
+}
